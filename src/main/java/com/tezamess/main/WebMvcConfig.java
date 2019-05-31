836271d729f9b76b3c7edd9c592bac617c27eaaf
Minh Trí đẹp trai <nguyenminhtri455@gmail.com>
2019-05-31 21:05:19 +0700

    them config url server

--- src/main/java/com/tezamess/main/WebMvcConfig.java
+++ src/main/java/com/tezamess/main/WebMvcConfig.java
@@ -17,7 +17,7 @@ public class WebMvcConfig implements WebMvcConfigurer{
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-      registry.addResourceHandler("/profile/**").addResourceLocations("file:profile/");
+      registry.addResourceHandler("/profile/**").addResourceLocations("file:/home/jboss/profile/");
     }
     
     
