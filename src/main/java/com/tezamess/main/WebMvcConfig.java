5ab4403aa58f997bbdd1c80b46ad719a34a3c6f1
Minh Trí đẹp trai <nguyenminhtri455@gmail.com>
2019-05-31 20:26:22 +0700

    dang ky url public image

--- src/main/java/com/tezamess/main/WebMvcConfig.java
+++ src/main/java/com/tezamess/main/WebMvcConfig.java
@@ -4,6 +4,7 @@ package com.tezamess.main;
 import com.tezamess.component.HandlerInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,6 +14,13 @@ public class WebMvcConfig implements WebMvcConfigurer{
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new HandlerInterceptor());
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+      registry.addResourceHandler("/profile/**").addResourceLocations("file:profile/");
+    }
+    
+    
     
     
     
